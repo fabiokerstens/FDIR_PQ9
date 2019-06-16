49f68d6b723a4ed2293ef01e5f0d82df5ae97405
Katy <k8ty.b@hotmail.co.uk>
2019-06-16 16:34:44 +0200

    Graphing errors

--- PQ_integretion_testing/error_graphs.py
+++ PQ_integretion_testing/error_graphs.py
@@ -1,23 +1,31 @@
 # Error graphs
 
 import json
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 plt.xlabel('Memory address')
 plt.ylabel('Error')
+plt.ylim(bottom = -0.5)
+plt.ylim(top = 1.5)
 
 
-json_bad = r"/home/katy/FDIR_PQ9/PQ_integretion_testing/bad_addresses.json".replace('\\', '/')
-json_good = r"/home/katy/FDIR_PQ9/PQ_integretion_testing/good_addresses.json".replace('\\', '/')
+
+# json_bad = r"/home/katy/FDIR_PQ9/PQ_integretion_testing/bad_addresses.json".replace('\\', '/')
+# json_good = r"/home/katy/FDIR_PQ9/PQ_integretion_testing/good_addresses.json".replace('\\', '/')
+
+json_bad = r"C:\Users\Katy\Documents\Masters\Q3 Microsat Engineering\FDIR_PQ9\PQ_integretion_testing/bad_addresses.json".replace('\\', '/')
+json_good = r"C:\Users\Katy\Documents\Masters\Q3 Microsat Engineering\FDIR_PQ9\PQ_integretion_testing/good_addresses.json".replace('\\', '/')
 
 bad_addresses = json.load(open(json_bad.replace('\\', '/')))
 good_addresses = json.load(open(json_good.replace('\\', '/')))
 
-for i in len(bad_addresses):
-    plt.scatter(bad_addresses[i], 1, c='r', s=4)
 
-for i in len(good_addresses):
-    plt.scatter(bad_addresses[i], 0, c='r', s=4)
+plt.scatter(bad_addresses, np.ones(len(bad_addresses)), c='r', s=4)
+plt.scatter(good_addresses, np.zeros(len(good_addresses)), c='b', s=4)
+
+# for i in len(good_addresses):
+#     plt.scatter(bad_addresses[i], 0, c='r', s=4)
 
 
 
