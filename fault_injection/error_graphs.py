c5c1b82753962e2e9922bc3f325e4883dc8cb39f
Katy <k8ty.b@hotmail.co.uk>
2019-06-18 14:36:21 +0200

    New adb graph

--- fault_injection/error_graphs.py
+++ fault_injection/error_graphs.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # Loading .json files
 # json_data_errors = r"address_logs/data_errors.json".replace('\\', '/')
-json_missing_hk_packets = r"address_logs/missing_hk_packets.json".replace('\\', '/')
+# json_missing_hk_packets = r"address_logs/missing_hk_packets.json".replace('\\', '/')
 json_missing_ft_packets = r"address_logs/missing_ft_packets.json".replace('\\', '/')
 json_no_errors = r"address_logs/no_errors.json".replace('\\', '/')
 
@@ -21,12 +21,12 @@ plt.ylim(top = 1.5)
 no_errors = json.load(open(json_no_errors.replace('\\', '/')))
 # data_errors = json.load(open(json_data_errors.replace('\\', '/')))
 missing_ft_packets = json.load(open(json_missing_ft_packets.replace('\\', '/')))
-missing_hk_packets = json.load(open(json_missing_hk_packets.replace('\\', '/')))
+# missing_hk_packets = json.load(open(json_missing_hk_packets.replace('\\', '/')))
 
 # Plotting errors for different memory location
 # plt.scatter(data_errors, np.ones(len(data_errorss)), c='k', s=4)
 plt.scatter(no_errors, np.zeros(len(no_errors)), c='b', s=4)
 plt.scatter(missing_ft_packets, np.ones(len(missing_ft_packets)), c='r', s=4)
-
+print(len(no_errors)+len(missing_ft_packets))
 
 plt.show()
